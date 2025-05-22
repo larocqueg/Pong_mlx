1620591c6e584112aba802774d72953e7597840e
larocqueg <portugalgabriel2021@gmail.com>
2025-05-22 20:51:36 +0100

    pong

--- srcs/init_window.c
+++ srcs/init_window.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   init_window.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rafaelfe <rafaelfe@student.42porto.com>    +#+  +:+       +#+        */
+/*   By: gde-la-r <gde-la-r@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/03/08 18:26:08 by rafaelfe          #+#    #+#             */
-/*   Updated: 2025/05/14 17:52:13 by rafaelfe         ###   ########.fr       */
+/*   Created: 2025/05/22 20:47:06 by gde-la-r          #+#    #+#             */
+/*   Updated: 2025/05/22 20:50:05 by gde-la-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,7 @@ void	init_window(t_cub *cub)
 {
 	(*cub).mlx_ptr = mlx_init();
 	(*cub).win_ptr = mlx_new_window((*cub).mlx_ptr, SCREEN_SIZE_X, SCREEN_SIZE_Y, "cub3d");
-
+	cub->image.img = mlx_new_image(cub->mlx_ptr, SCREEN_SIZE_X, SCREEN_SIZE_Y);
+	cub->image.addr = mlx_get_data_addr(cub->image.img, &cub->image.bits_per_pixel,
+			&cub->image.line_length, &cub->image.endian);
 }
